c36362cc2b464ce161fefb806195c331cac54657
Slava <v.zhevaga@gmail.com>
2024-06-02 13:58:05 +0200

    uncomented code

---
+++ sprint_04/task_01.js
@@ -0,0 +1,13 @@
+// itgid.info - курс Node.js
+
+
+// Task 01
+// Создайте анонимный модуль в данном файле, который принимает аргумент - имя файла. Возвращает содержимое файла. Пример вызова в index.js
+
+const fs = require('fs');
+const path = require('path');
+
+module.exports = function (fileName) {
+    
+}
+
